5350496f531c6b6d80b8b3e55871e17dbac92ba4
soohyun <vivian0304@naver.com>
2022-07-21 10:51:27 +0900

    add new files

---
+++ app.py
@@ -0,0 +1,28 @@
+from workerA import add_nums
+from workerB import sub_nums
+from flask import (
+   Flask,
+   request,
+   jsonify,
+)
+
+app = Flask(__name__)
+
+@app.route("/add")
+def add():
+    first_num = request.args.get('first_num')
+    second_num = request.args.get('second_num')
+    print(first_num)
+    result = add_nums.delay(first_num, second_num )
+    return jsonify({'result': result}), 200
+
+
+@app.route("/subtract")
+def subtract():
+    first_num = request.args.get('first_num')
+    second_num = request.args.get('second_num')
+    result = sub_nums.delay(first_num, second_num )
+    return jsonify({'result': result}), 200
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
